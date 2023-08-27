0f89932b583e8279b7d7ba0f13d6adb7f81cb885
Hibari <jongxiankhoo@gmail.com>
2023-08-28 01:02:54 +0800

    feat: install daisyui

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Portfolio Blog',
+  author: 'Hibari Khoo',
+  headerTitle: 'Portfolio',
+  description: 'My portfolio',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
